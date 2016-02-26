2097ff4566a9f98511b65f4398f2f6540078e495
tejobr <tejobr@yahoo.com.br>
2016-02-26 13:00:38 -0300

    teste

---
+++ H2O.LojaVirtual.Web/Controllers/LojaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace H2O.LojaVirtual.Web.Controllers
+{
+    public class LojaController : Controller
+    {
+        // GET: Loja
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
